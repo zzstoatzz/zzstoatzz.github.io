3424e942d98d8bc21a19e020e1c455d9dd087f6e
zzstoatzz <thrast36@gmail.com>
2026-03-03 12:24:04 -0600

    Revert "Refine firehose entropy into settings-aware weather field"
    This reverts commit 9e2cde795b83858064d44922bfde93eb9af9392d.

--- public/js/particles/config.js
+++ public/js/particles/config.js
@@ -17,7 +17,7 @@ export const RANGES = {
 	PARTICLE_COLOR: { default: "#64ffda" },
 	ENABLE_VORTEX_FORCE: { default: false },
 	FIREHOSE_ENTROPY: { default: false },
-	FIREHOSE_ENTROPY_GAIN: { min: 0.1, max: 1.5, step: 0.05, default: 0.75 },
+	FIREHOSE_ENTROPY_GAIN: { min: 0.1, max: 2, step: 0.05, default: 1.0 },
 };
 
 // Generate default settings object from the RANGES
